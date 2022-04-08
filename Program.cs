11a4266b0816fb3dca74d2a01549748832bb6989
bruno0011 <brunosouzviana@hotmail.com>
2022-04-08 19:30:32 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿
+int numero;
+Console.Write("Digite um número: ");
+numero = Convert.ToInt32(Console.ReadLine());
+if (numero < 0)
+{
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("Você digitou um número negativo.");
+
+}
+Console.ResetColor();
